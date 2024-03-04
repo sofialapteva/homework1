0098f79cf043b4aa51545806811c2751d18674dc
sofialapteva <slapteva@rzd.dev>
2024-03-04 22:09:11 +0300

    initial commit

---
+++ lib/homework1.ex
@@ -0,0 +1,18 @@
+defmodule Homework1 do
+  @moduledoc """
+  Documentation for `Homework1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Homework1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/homework1_test.exs
@@ -0,0 +1,8 @@
+defmodule Homework1Test do
+  use ExUnit.Case
+  doctest Homework1
+
+  test "greets the world" do
+    assert Homework1.hello() == :world
+  end
+end
