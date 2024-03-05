ae34ff3d5d55614324512f47d402b47975027f23
sofialapteva <slapteva@rzd.dev>
2024-03-05 10:01:12 +0300

    complete first iteration

--- lib/homework1.ex
+++ lib/homework1.ex
@@ -1,18 +1,50 @@
 defmodule Homework1 do
   @moduledoc """
-  Documentation for `Homework1`.
+  This module implements the simple calculation function.
+  You can use the Homework1.calculate/1 function to add, remove, multiply and divide.
   """
 
   @doc """
-  Hello world.
+  Calculate the result of an expression:
 
   ## Examples
-
-      iex> Homework1.hello()
-      :world
+      iex> Homework1.calculate "1 + 1"
+      2.0
+      iex> Homework1.calculate "12 - 76"
+      -64.0
+      iex> Homework1.calculate "11 * 9"
+      99.0
+      iex> Homework1.calculate "11 / 2"
+      5.5
 
   """
-  def hello do
-    :world
+
+  @spec calculate(String.t()) :: number()
+  def calculate(expression) when is_binary(expression) do
+    expression
+    |> String.replace(" ", "")
+    |> Float.parse()
+    |> compute()
+  end
+
+  @spec compute({Float.t(), String.t()}) :: number()
+  defp compute({a, "+" <> rest}) do
+    {b, _} = Float.parse(rest)
+    a + b
+  end
+
+  defp compute({a, "-" <> rest}) do
+    {b, _} = Float.parse(rest)
+    a - b
+  end
+
+  defp compute({a, "*" <> rest}) do
+    {b, _} = Float.parse(rest)
+    a * b
+  end
+
+  defp compute({a, "/" <> rest}) do
+    {b, _} = Float.parse(rest)
+    a / b
   end
 end
--- test/homework1_test.exs
+++ test/homework1_test.exs
@@ -2,7 +2,27 @@ defmodule Homework1Test do
   use ExUnit.Case
   doctest Homework1
 
-  test "greets the world" do
-    assert Homework1.hello() == :world
+  test "addition" do
+    assert Homework1.calculate("1+1") === 2.0
+    assert Homework1.calculate("1+0") === 1.0
+    assert Homework1.calculate("-1+0") === -1.0
+  end
+
+  test "removal" do
+    assert Homework1.calculate("5-1") === 4.0
+    assert Homework1.calculate("1-5") === -4.0
+    assert Homework1.calculate("1-0") === 1.0
+  end
+
+  test "multiply" do
+    assert Homework1.calculate("2*5") === 10.0
+    assert Homework1.calculate("-2*5") === -10.0
+    assert Homework1.calculate("-2*0") === 0.0
+  end
+
+  test "divide" do
+    assert Homework1.calculate("2/5") === 0.4
+    assert Homework1.calculate("-2.5/5") === -0.5
+    assert Homework1.calculate("-2/1") === -2.0
   end
 end
